ad35e856d083eebee12ed8be4b9e0c1058a40c29
ZeldaRose93 <ZeldaRose93@gmail.com>
2022-12-21 10:39:29 -0600

    Added a normalization function mean_removal to the tests and implementation

--- src/preprocessing/preprocessing_base.py
+++ src/preprocessing/preprocessing_base.py
@@ -10,8 +10,48 @@ import numpy as np
 
 
 def binarizer(input_data, threshold):
+    """
+    Changes values in a np array to either 1 or 0 based on the threshold value.
+
+    Args:
+        input_data (np.array): The array of values you want to binarize
+        threshold (float): the value where numbers will switch from 0 to 1.
+
+    Returns:
+        a binarized numpy array.
+    """
     output_data = input_data
     output_data[output_data > threshold] = 1 + threshold
     output_data[output_data <= threshold] = 0 + threshold
     output_data = output_data - threshold
     return output_data
+
+
+def mean_removal(input_data, axis=1):
+    """
+    Returns the input dataframe after normalizing the values by column.
+    Uses the formula z = (x - mu) / sigma
+
+    Args:
+        input_data: Numpy array of the values to separate
+        axis=1: Sets default normalization axis to columns
+
+    Returns: Numpy array with same dimensions as the input and values
+             standardized to mean and std.
+    """
+    if axis == 1:
+        input_columns = np.split(input_data, input_data.shape[1], axis=axis)
+        for index in range(len(input_columns)):
+            inp_mean = input_columns[index].mean()
+            inp_std = input_columns[index].std()
+            input_columns[index] = (input_columns[index] - inp_mean) / inp_std
+        output = np.concatenate(input_columns, axis=1)
+        return output
+    elif axis == 0:
+        input_rows = np.split(input_data, input_data.shape[0], axis=axis)
+        for index in range(len(input_rows)):
+            inp_mean = input_rows[index].mean()
+            inp_std = input_rows[index].std()
+            input_rows[index] = (input_rows[index] - inp_mean) / inp_std
+        output = np.concatenate(input_rows, axis=0)
+        return output
--- tests/preprocessing/test_preprocessing.py
+++ tests/preprocessing/test_preprocessing.py
@@ -10,13 +10,13 @@ import os
 import sys
 
 import numpy as np
-from numpy.testing import assert_array_equal
+import numpy.testing as np_test
 import pytest
 
 sys.path.insert(0,
                 os.path.abspath(os.path.join(os.path.dirname(__file__),'..', '..')))
 from src.preprocessing.preprocessing_base import binarizer
-
+from src.preprocessing.preprocessing_base import mean_removal
 
 
 class TestBinarizer:
@@ -33,6 +33,79 @@ class TestBinarizer:
                             [1., 0., 0.]])
         assert actual.shape == expected.shape, \
             "Input and Output are different sizes."
-        assert_array_equal(actual,
-                           expected,
-                           "Output array does not match expected value")
+        np_test.assert_array_equal(actual,
+                                   expected,
+                                   "Actual does not match expected")
+
+
+class TestMeanRemoval:
+    def test_mean_removal_shape(self):
+        input_data = np.array([[5.1, -2.9, 3.3],
+                               [-1.2, 7.8, -6.1],
+                               [3.9, 0.4, 2.1],
+                               [7.3, -9.9, -4.5]])
+        actual = mean_removal(input_data)
+        expected = np.array([[0.42462551, -0.2748757,  1.13244172],
+                            [-1.59434861,  1.40579288, -1.18167831],
+                            [0.04005901,  0.24346134,  0.83702214],
+                            [1.12966409, -1.37437851, -0.78778554]])
+        assert actual.shape == expected.shape, \
+            "Input and Output are different sizes."
+
+    def test_mean_removal_std(self):
+        input_data = np.array([[5.1, -2.9, 3.3],
+                               [-1.2, 7.8, -6.1],
+                               [3.9, 0.4, 2.1],
+                               [7.3, -9.9, -4.5]])
+        actual = mean_removal(input_data)
+        act_std = actual.std(axis=0)
+        expected_std = np.array([1., 1., 1.])
+        np_test.assert_allclose(act_std,
+                                expected_std,
+                                rtol=1e-04,
+                                err_msg="Std not normalized.")
+
+    def test_mean_removal_array(self):
+        input_data = np.array([[5.1, -2.9, 3.3],
+                               [-1.2, 7.8, -6.1],
+                               [3.9, 0.4, 2.1],
+                               [7.3, -9.9, -4.5]])
+        actual = mean_removal(input_data)
+        expected = np.array([[0.42462551, -0.2748757,  1.13244172],
+                            [-1.59434861,  1.40579288, -1.18167831],
+                            [0.04005901,  0.24346134,  0.83702214],
+                            [1.12966409, -1.37437851, -0.78778554]])
+
+        np_test.assert_allclose(actual,
+                                expected,
+                                rtol=1e-07,
+                                err_msg="Actual and expected values vary.")
+
+    def test_mean_removal_std_rowwise(self):
+        input_data = np.array([[5.1, -2.9, 3.3],
+                               [-1.2, 7.8, -6.1],
+                               [3.9, 0.4, 2.1],
+                               [7.3, -9.9, -4.5]])
+        actual = mean_removal(input_data, axis=0)
+        act_std = actual.std(axis=1)
+        expected_std = np.array([1., 1., 1., 1.])
+        np_test.assert_allclose(act_std,
+                                expected_std,
+                                rtol=1e-04,
+                                err_msg="Std not normalized.")
+
+    def test_mean_removal_array_rowwise(self):
+        input_data = np.array([[5.1, -2.9, 3.3],
+                               [-1.2, 7.8, -6.1],
+                               [3.9, 0.4, 2.1],
+                               [7.3, -9.9, -4.5]])
+        actual = mean_removal(input_data, axis=0)
+        expected = np.array([[0.95330018, -1.3813125, 0.42801232],
+                            [-0.237419, 1.32607198, -1.08865298],
+                            [1.23624092, -1.21291562, -0.0233253],
+                            [1.34594316, -1.04890743, -0.29703573]])
+
+        np_test.assert_allclose(actual,
+                                expected,
+                                rtol=1e-07,
+                                err_msg="Actual and expected values vary.")
